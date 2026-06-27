04a17badda28451d7fec6506857ed42d0258a6fe
Ganzullo <ganzouni@gmail.com>
2026-06-26 21:49:32 -0400

    a

--- components/dashboard/oferta-detalle.tsx
+++ components/dashboard/oferta-detalle.tsx
@@ -14,6 +14,7 @@ import {
 import { Button } from "@/components/ui/button"
 import { BLOQUES_FILTRO } from "@/lib/dashboard-data"
 import { cn } from "@/lib/utils"
+import Link from "next/link"
 
 interface Oferta {
   id: string
